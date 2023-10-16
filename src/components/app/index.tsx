21f5371afd4602b77d2379888661ff0f1d49df3d
Oleg Zharov <levineye@gmail.com>
2023-10-16 21:46:45 +0300

    feat: добавить scss и директорию компонентов

---
+++ src/components/app/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC, ReactElement } from 'react';
+
+import styles from './index.module.scss';
+
+const App: FC = (): ReactElement => {
+  return <div className={styles.app}>App</div>;
+};
+
+export default App;
