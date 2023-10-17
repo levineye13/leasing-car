97d5c0ec4ab6d817b1eca011f8664ad4d433d769
Oleg Zharov <levineye@gmail.com>
2023-10-17 21:36:58 +0300

    feat: добавить компонент точек страниц слайдера

---
+++ src/components/points-slider/index.tsx
@@ -0,0 +1,31 @@
+import React, { FC, ReactElement } from 'react';
+
+import styles from './index.module.scss';
+
+interface IPointsSlider {
+  readonly length: number;
+  readonly activeIndex: number;
+  readonly className?: string;
+}
+
+const PointsSlider: FC<IPointsSlider> = ({
+  length,
+  activeIndex,
+  className,
+}): ReactElement => {
+  return (
+    <div className={`${styles.points} ${className}`}>
+      <ul className={styles.list}>
+        {new Array(length).fill(null).map((_, index) => (
+          <li
+            className={`${styles.item} ${
+              index === activeIndex ? styles.item_active : ''
+            }`}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PointsSlider;
