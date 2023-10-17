15f3bd0fb6e60b35fd8137c33c464dc8f7e62822
Oleg Zharov <levineye@gmail.com>
2023-10-18 01:14:59 +0300

    feat: добавить в app работу с модалкой и страницу со слайдером

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,9 +1,33 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 
+import Header from '../header';
+import Menu from '../burger-menu';
+import SliderPage from '../../pages/slider';
+import Background from '../background';
 import styles from './index.module.scss';
 
 const App: FC = (): ReactElement => {
-  return <div className={styles.app}>App</div>;
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleOpenMenu = () => {
+    setMenuIsOpen(true);
+  };
+
+  const handleCloseMenu = () => {
+    setMenuIsOpen(false);
+  };
+
+  return (
+    <div className={styles.app}>
+      <Header onOpenMenu={handleOpenMenu} />
+      <main className={styles.main}>
+        <SliderPage />
+      </main>
+      <Background isOpen={menuIsOpen} onClose={handleCloseMenu}>
+        <Menu onClose={handleCloseMenu} />
+      </Background>
+    </div>
+  );
 };
 
 export default App;
