16d4b4274399fe3286316bf66831e99f058bba63
Oleg Zharov <levineye@gmail.com>
2023-10-19 16:21:56 +0300

    feat: добавить иконки соцсетей

--- src/images/index.tsx
+++ src/images/index.tsx
@@ -1,7 +1,11 @@
 import HeaderLogo from './header-logo.jpg';
 import Sum from './sum.svg';
+import Whatsapp from './whatsapp.svg';
+import Telegram from './telegram.svg';
 
 export const images = {
   headerLogo: HeaderLogo,
   sum: Sum,
+  whatsapp: Whatsapp,
+  telegram: Telegram,
 };
