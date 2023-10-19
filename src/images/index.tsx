6afa6af7974830fc6581e458a6010fa22450f0a8
Oleg Zharov <levineye@gmail.com>
2023-10-19 18:56:46 +0300

    feat: добавить компонент логотипа svg с адаптивным цветом

--- src/images/index.tsx
+++ src/images/index.tsx
@@ -1,10 +1,8 @@
-import HeaderLogo from './header-logo.jpg';
 import Sum from './sum.svg';
 import Whatsapp from './whatsapp.svg';
 import Telegram from './telegram.svg';
 
 export const images = {
-  headerLogo: HeaderLogo,
   sum: Sum,
   whatsapp: Whatsapp,
   telegram: Telegram,
